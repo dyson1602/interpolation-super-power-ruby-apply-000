ab020dd56b34d34cd71d26de19fa66ad4c828141
Christopher Clark <cclark000@citymail.cuny.edu>
2020-08-21 18:57:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,4 +5,7 @@ def display_rainbow(colors)
   puts "R: #{colors[0]"
   puts "O: #{colors[1]"
   puts "Y: #{colors[2]"
+  puts "G: #{colors[3]"
+  puts "Y: #{colors[2]"
+  puts "Y: #{colors[2]"
   puts "Y: #{colors[2]"
